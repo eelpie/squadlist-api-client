5b382ce019c1094417cbe3021cbf403b3edd16e0
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-06-19 16:48:57 +0100

    Tariff does not have an expires field.

--- src/main/java/uk/co/squadlist/web/model/Tariff.java
+++ src/main/java/uk/co/squadlist/web/model/Tariff.java
@@ -1,57 +1,45 @@
 package uk.co.squadlist.web.model;
 
-import java.util.Date;
-
 public class Tariff {
 
-	private String id;
-	private String name;
-	Integer duration;
-	Date expires;
+    private String id;
+    private String name;
+    private Integer duration;
 
-	public Tariff() {
-	}
+    public Tariff() {
+    }
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Integer getDuration() {
-		return duration;
-	}
+    public Integer getDuration() {
+        return duration;
+    }
 
-	public void setDuration(Integer duration) {
-		this.duration = duration;
-	}
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
 
-	public Date getExpires() {
-		return expires;
-	}
-
-	public void setExpires(Date expires) {
-		this.expires = expires;
-	}
-
-	@Override
-	public String toString() {
-		return "Tariff{" +
-						"id='" + id + '\'' +
-						", name='" + name + '\'' +
-						", duration=" + duration +
-						", expires=" + expires +
-						'}';
-	}
+    @Override
+    public String toString() {
+        return "Tariff{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
 
 }
